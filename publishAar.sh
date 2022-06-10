ead36b8737a6495f3e2579cc9dd1dafecb95589b
tyranron <tyranron@gmail.com>
2022-06-10 16:47:05 +0200

    Add EditorConfig and strip unused Linux ARM

--- publishAar.sh
+++ publishAar.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 while IFS="=" read -r key value; do
-    case "$key" in
-      "WEBRTC_SEMANTIC_VERSION") VERSION="$value" ;;
-    esac
-  done < ./VERSION
+  case "$key" in
+    "WEBRTC_SEMANTIC_VERSION") VERSION="$value" ;;
+  esac
+done < ./VERSION
 
-AAR_URL=https://github.com/instrumentisto/libwebrtc-bin/releases/download/${VERSION}/libwebrtc-android.tar.gz
+AAR_URL="https://github.com/instrumentisto/libwebrtc-bin/releases/download/${VERSION}/libwebrtc-android.tar.gz"
+echo "AAR_URL=${AAR_URL}"
 
-echo AAR_URL=${AAR_URL}
+mkdir -p package/
+cd package/
 
-mkdir -p package
-cd package
-
-curl -L -O ${AAR_URL}
+curl -L -O "${AAR_URL}"
 tar xf libwebrtc-android.tar.gz
 
-mvn install:install-file \
-    -Dfile=aar/libwebrtc.aar \
-    -Dpackaging=aar \
-    -Dversion=${VERSION} \
-    -DgroupId=com.github.instrumentisto \
-    -DartifactId=libwebrtc-bin
-
+mvn install:install-file -Dfile=aar/libwebrtc.aar \
+                         -Dpackaging=aar \
+                         -Dversion=${VERSION} \
+                         -DgroupId=com.github.instrumentisto \
+                         -DartifactId=libwebrtc-bin
