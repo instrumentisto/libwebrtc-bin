32c6f23e607af96a5a7dce88c0ce86d3a1acebe6
tyranron <tyranron@gmail.com>
2022-06-15 11:35:07 +0200

    Test errors in build script, vol.3

--- build.windows.ps1
+++ build.windows.ps1
@@ -5,6 +5,24 @@
 
 $ErrorActionPreference = "Stop"
 
+# Wraps native commands to handle their errors.
+# Original:
+#   https://stackoverflow.com/a/48999101/1828012
+#   https://stackoverflow.com/a/52784160/1828012
+function Exec
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+    [int[]]$SuccessCodes = @(0)
+
+  )
+  & $cmd
+  if ($SuccessCodes -notcontains $LastExitCode) {
+    exit $LastExitCode
+  }
+}
+
 # VERSIONファイル読み込み
 $lines = get-content VERSION
 foreach($line in $lines){
@@ -23,14 +41,14 @@ if (!(Test-Path vswhere.exe)) {
 # https://github.com/microsoft/vswhere/wiki/Find-VC
 $path = .\vswhere.exe -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
 if ($path) {
-  $batpath = join-path $path 'Common7\Tools\vsdevcmd.bat'
+  $batpath = Join-Path $path 'Common7\Tools\vsdevcmd.bat'
   if (Test-Path $batpath) {
     cmd /s /c """$batpath"" $args && set" | Where-Object { $_ -match '(\w+)=(.*)' } | ForEach-Object {
-      $null = new-item -force -path "Env:\$($Matches[1])" -value $Matches[2]
+      $null = New-Item -Force -Path "Env:\$($Matches[1])" -Value $Matches[2]
     }
   }
   # dbghelp.dll が無いと怒られてしまうので所定の場所にコピーする (管理者権限で実行する必要がある)
-  $debuggerpath = join-path $path "Common7\IDE\Extensions\TestPlatform\Extensions\Cpp\x64\dbghelp.dll"
+  $debuggerpath = Join-Path $path "Common7\IDE\Extensions\TestPlatform\Extensions\Cpp\x64\dbghelp.dll"
   if (!(Test-Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64")) {
     New-Item "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64" -ItemType Directory -Force
     Copy-Item $debuggerpath "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\dbghelp.dll"
@@ -52,12 +70,12 @@ $Env:PYTHONIOENCODING = "utf-8"
 # depot_tools
 if (Test-Path $DEPOT_TOOLS_DIR) {
   Push-Location $DEPOT_TOOLS_DIR
-    git checkout .
-    git clean -df .
-    git pull .
+    Exec { git checkout . }
+    Exec { git clean -df . }
+    Exec { git pull . }
   Pop-Location
 } else {
-  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  Exec { git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git }
 }
 
 $Env:PATH = "$DEPOT_TOOLS_DIR;$Env:PATH"
@@ -69,18 +87,18 @@ New-Item $WEBRTC_DIR -ItemType Directory -Force
 Push-Location $WEBRTC_DIR
 if (Test-Path .gclient) {
   Push-Location src
-  git checkout .
-  git clean -df
+    Exec { git checkout . }
+    Exec { git clean -df }
   Pop-Location
 
   Push-Location src\build
-  git checkout .
-  git clean -xdf
+    Exec { git checkout . }
+    Exec { git clean -xdf }
   Pop-Location
 
   Push-Location src\third_party
-  git checkout .
-  git clean -df
+    Exec { git checkout . }
+    Exec { git clean -df }
   Pop-Location
 } else {
   if (Test-Path $DEPOT_TOOLS_DIR\metrics.cfg) {
@@ -89,35 +107,36 @@ if (Test-Path .gclient) {
   if (Test-Path src) {
     Remove-Item src -Recurse -Force
   }
-  fetch --nohooks webrtc
+  Exec { fetch --nohooks webrtc }
 }
 
 if (!(Test-Path $BUILD_DIR)) {
-  mkdir $BUILD_DIR
+  New-Item $BUILD_DIR -ItemType Directory -Force
 }
 
-gclient sync --with_branch_heads -r $WEBRTC_COMMIT
-git apply --ignore-space-change -v $PATCH_DIR\webrtc_voice_engine.patch
-git apply --ignore-space-change -v $PATCH_DIR\win_dynamic_crt.patch
+Exec { gclient sync --with_branch_heads -r $WEBRTC_COMMIT }
+Exec { git apply --ignore-space-change -v $PATCH_DIR\webrtc_voice_engine.patch }
+Exec { git apply --ignore-space-change -v $PATCH_DIR\win_dynamic_crt.patch }
 Pop-Location
 
 Get-PSDrive
 
 Push-Location $WEBRTC_DIR\src
   # WebRTC Debugビルド x64
-  gn gen $BUILD_DIR\debug_x64 --args='treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true use_custom_libcxx=false'
-  ninja -C "$BUILD_DIR\debug_x64"
+  Exec { gn gen $BUILD_DIR\debug_x64 --args='treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true use_custom_libcxx=false' }
+  Exec { ninja -C "$BUILD_DIR\debug_x64" }
 
   # WebRTC Releaseビルド x64
-  gn gen $BUILD_DIR\release_x64 --args='is_debug=false treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true strip_debug_info=true symbol_level=0 use_custom_libcxx=false'
-  ninja -C "$BUILD_DIR\release_x64"
+  Exec { gn gen $BUILD_DIR\release_x64 --args='is_debug=false treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true strip_debug_info=true symbol_level=0 use_custom_libcxx=false' }
+  Exec { ninja -C "$BUILD_DIR\release_x64" }
 Pop-Location
 
 foreach ($build in @("debug_x64", "release_x64")) {
-  ninja -C "$BUILD_DIR\$build" audio_device_module_from_input_and_output
+  Exec { ninja -C "$BUILD_DIR\$build" audio_device_module_from_input_and_output }
 
   # このままだと webrtc.lib に含まれないファイルがあるので、いくつか追加する
   Push-Location $BUILD_DIR\$build\obj
+  Exec {
     lib.exe `
       /out:$BUILD_DIR\$build\webrtc.lib webrtc.lib `
       api\task_queue\default_task_queue_factory\default_task_queue_factory_win.obj `
@@ -129,6 +148,7 @@ foreach ($build in @("debug_x64", "release_x64")) {
       modules\audio_device\audio_device_module_from_input_and_output\core_audio_output_win.obj `
       modules\audio_device\windows_core_audio_utility\core_audio_utility_win.obj `
       modules\audio_device\audio_device_name\audio_device_name.obj
+  }
   Pop-Location
   Move-Item $BUILD_DIR\$build\webrtc.lib $BUILD_DIR\$build\obj\webrtc.lib -Force
 }
@@ -138,7 +158,7 @@ if (Test-Path $BUILD_DIR\package) {
   Remove-Item -Force -Recurse -Path $BUILD_DIR\package
 }
 New-Item $BUILD_DIR\package\webrtc\include -ItemType Directory -Force
-robocopy "$WEBRTC_DIR\src" "$BUILD_DIR\package\webrtc\include" *.h *.hpp /S /NP /NS /NC /NFL /NDL
+Exec { robocopy "$WEBRTC_DIR\src" "$BUILD_DIR\package\webrtc\include" *.h *.hpp /S /NP /NS /NC /NFL /NDL } -SuccessCodes @(1)
 
 # ライブラリディレクトリ作成
 New-Item $BUILD_DIR\package\webrtc\debug -ItemType Directory -Force
@@ -149,7 +169,7 @@ $WEBRTC_VERSION | Out-File $BUILD_DIR\package\webrtc\VERSION
 
 # ライセンス生成 (x64)
 Push-Location $WEBRTC_DIR\src
-  vpython tools_webrtc\libs\generate_licenses.py --target :webrtc "$BUILD_DIR\" "$BUILD_DIR\debug_x64" "$BUILD_DIR\release_x64"
+  Exec { vpython tools_webrtc\libs\generate_licenses.py --target : webrtc "$BUILD_DIR\" "$BUILD_DIR\debug_x64" "$BUILD_DIR\release_x64" }
 Pop-Location
 Copy-Item "$BUILD_DIR\LICENSE.md" "$BUILD_DIR\package\webrtc\NOTICE"
 
@@ -165,5 +185,5 @@ if (Test-Path $PACKAGE_DIR\libwebrtc-windows-x64.tar.gz) {
   Remove-Item -Force -Path $PACKAGE_DIR\libwebrtc-windows-x64.tar.gz
 }
 Push-Location $BUILD_DIR\package\webrtc
-  tar -czvf $PACKAGE_DIR\libwebrtc-windows-x64.tar.gz include debug release NOTICE VERSION
+  Exec { tar -czvf $PACKAGE_DIR\libwebrtc-windows-x64.tar.gz include debug release NOTICE VERSION }
 Pop-Location
