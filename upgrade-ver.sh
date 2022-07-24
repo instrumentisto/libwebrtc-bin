dbc26d445316de8355e10077dc20e79d3b8cb551
tyranron <tyranron@gmail.com>
2022-07-24 19:08:49 +0300

    Corrections

--- upgrade-ver.sh
+++ upgrade-ver.sh
@@ -1,27 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 res=$(curl 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1&offset=0')
-if [[ "$res" != *"\"version\""* ]]; then
-    echo -e "There is wrong \`JSON\` response, no \`version\` field there.\nCheck the \`Chrome API\`, fix this \`.sh\` if needed and/or try again."
-    exit 1
+
+newVersion=$(printf "$res" | jq -r '.[0].version')
+if [[ "$newVersion" = "null" ]]; then
+  echo -e "Wrong JSON response, no \`version\` field found.\nCheck the Chrome API and fix this \`.sh\` if needed and/or try again."
+  exit 1
 fi
-if [[ "$res" != *"\"webrtc\""* ]]; then
-    echo -e "There is wrong \`JSON\` response, no \`webrtc\` field there.\nCheck the \`Chrome API\`, fix this \`.sh\` if needed and/or try again."
-    exit 1
+
+newCommit=$(printf "$res" | jq -r '.[0].hashes.webrtc')
+if [[ "$newCommit" = "null" ]]; then
+  echo -e "Wrong JSON response, no \`hashes.webrtc\` field found.\nCheck the Chrome API and fix this \`.sh\` if needed and/or try again."
+  exit 1
 fi
-echo "$res" |
-jq -r '"version:\(.[0].version)\nwebrtc:\(.[0].hashes.webrtc)"' |
-while read data; do
-    name=$(echo "$data" | cut -d':' -f1)
-    value=$(echo "$data" | cut -d':' -f2)
-    field=$([[ $name = version ]] && echo "WEBRTC_VERSION" || echo "WEBRTC_COMMIT")
-    grep -n $field ./VERSION |
-    cut -d':' -f1 |
-    {
-        read line
-        sed -i "${line}s|.*|${field}=${value}|g" ./VERSION
-    }
-    if [ $name = version ]; then
-        echo "::set-output name=$(echo $name)::$(echo $value)"
-    fi
-done
+
+sed -i.bk "s/^WEBRTC_VERSION=.*$/WEBRTC_VERSION=$newVersion/g" ./VERSION
+sed -i.bk "s/^WEBRTC_COMMIT=.*$/WEBRTC_COMMIT=$newCommit/g" ./VERSION
+
+echo "::set-output name=version::$newVersion"
+echo "::set-output name=commit::$newCommit"
