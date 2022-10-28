25316f8f50900cd0af6ed811ea9ba7c51993809a
evdokimovs <evdokimovs@protonmail.ch>
2022-10-28 10:50:26 +0200

    Add PodSpec generation script

--- scripts/generate_podspec.sh
+++ scripts/generate_podspec.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
+# usage: generate_podspec.sh OUTPUT_FILE VERSION
+
+DIR=$(cd $1 && pwd)
+WEBRTC_VERSION=$2
+
+echo Output PodSpec to $DIR/MedeaWebRTC.podspec
+cat << EOF > $DIR/MedeaWebRTC.podspec
 Pod::Spec.new do |spec|
     spec.name         = "MedeaWebRTC"
-    spec.version      = "104.5112.05"
+    spec.version      = "$WEBRTC_VERSION"
     spec.summary      = "WebRTC pre-compiled library for Darwin used by Instrumentisto Flutter-WebRTC."
   
     spec.homepage     = "https://github.com/instrumentisto/libwebrtc-bin"
@@ -9,6 +18,7 @@ Pod::Spec.new do |spec|
     spec.ios.deployment_target = '10.0'
     spec.osx.deployment_target = '10.11'
   
-    spec.source       = { :http => "https://github.com/webrtc-sdk/Specs/releases/download/104.5112.05/WebRTC.xcframework.zip" }
+    spec.source       = { :http => "https://github.com/webrtc-sdk/Specs/releases/download/$WEBRTC_VERSION/WebRTC.xcframework.zip" }
     spec.vendored_frameworks = "WebRTC.xcframework"
 end
+EOF
