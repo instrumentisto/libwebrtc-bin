7debe1eeee7851b63498bd3ded8646de7aca200d
evdokimovs <evdokimovs@protonmail.ch>
2022-11-01 11:14:26 +0100

    Move PodSpec generation to check-new-ver.sh

--- upgrade-ver.sh
+++ upgrade-ver.sh
@@ -20,5 +20,22 @@ sed -i.bk -e "s/^WEBRTC_VERSION=.*$/WEBRTC_VERSION=$newVersion/g" \
           -e "s/^WEBRTC_COMMIT=.*$/WEBRTC_COMMIT=$newCommit/g" \
     ./VERSION
 
+cat << EOF > ./MedeaWebRTC.podspec
+Pod::Spec.new do |spec|
+  spec.name         = "MedeaWebRTC"
+  spec.version      = "$newVersion"
+  spec.summary      = "WebRTC pre-compiled library for Darwin used by Instrumentisto Flutter-WebRTC."
+
+  spec.homepage     = "https://github.com/instrumentisto/libwebrtc-bin"
+  spec.license      = { :type => 'BSD', :file => 'WebRTC.xcframework/LICENSE' }
+  spec.author       = { 'Instrumentisto Team' => 'developer@instrumentisto.com' }
+  spec.ios.deployment_target = '10.0'
+  spec.osx.deployment_target = '10.11'
+
+  spec.source       = { :http => "https://github.com/instrumentisto/libwebrtc-bin/releases/download/$newVersion/libwebrtc-ios.zip" }
+  spec.vendored_frameworks = "WebRTC.xcframework"
+end
+EOF
+
 echo "::set-output name=version::$newVersion"
 echo "::set-output name=commit::$newCommit"
