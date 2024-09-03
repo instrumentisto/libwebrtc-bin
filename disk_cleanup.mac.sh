e6e656c94715e84a9650dd591b80ddd3db0f698a
tyranron <tyranron@gmail.com>
2024-09-03 16:35:45 +0300

    Bikeshed

--- disk_cleanup.mac.sh
+++ disk_cleanup.mac.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
 
-# Display disk usage before cleanup
 df -h
 
-# Remove Homebrew cache and unused packages
+# Homebrew cache and unused packages
 brew cleanup
 brew autoremove
 rm -rf $(brew --cache)
 
-# Remove XCode simulators
+# XCode simulators
 sudo rm -rf ~/Library/Developer/Xcode/iOS\ DeviceSupport/*
 sudo rm -rf ~/Library/Developer/CoreSimulator/Caches/*
 sudo rm -rf ~/Library/Developer/CoreSimulator/Devices/*
 
-# Remove .NET SDK and runtimes
+# .NET SDK and runtimes
 sudo rm -rf /usr/local/share/dotnet
 sudo rm -rf ~/.dotnet
 
-# Remove Haskell (GHC)
+# Haskell (GHC)
 sudo rm -rf ~/.ghcup
 sudo rm -rf /opt/ghc
 
-# Remove Swift
+# Swift
 sudo rm -rf /usr/share/swift
 
-# Remove Android SDK
+# Android SDK
 sudo rm -rf ~/Library/Android/sdk
 
-# Remove Python environments and pip cache
+# Python environments and pip cache
 sudo rm -rf ~/.pyenv
 sudo rm -rf ~/Library/Caches/pip
 
-# Remove Node.js and npm cache
+# Node.js and NPM cache
 sudo rm -rf ~/.npm
 sudo rm -rf ~/.node-gyp
 sudo rm -rf ~/.nvm
 
-# Remove Ruby gems and cache
+# Ruby gems and cache
 sudo rm -rf ~/.gem
 sudo rm -rf ~/.rbenv
 
-# Remove temporary files
+# Temporary files
 sudo rm -rf /tmp/*
 sudo rm -rf /private/var/tmp/*
 sudo rm -rf /var/folders/*
 
-# Display disk usage after cleanup
 df -h
