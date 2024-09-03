82da00f50a6caf909eb05c5b9917425e738570cd
evdokimovs <evdokimovs@protonmail.ch>
2024-09-03 11:05:53 +0200

    ci: add cleanup for MacOS

---
+++ disk_cleanup_mac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+df -h
+
+# Cache 済み Docker Image の削除
+docker rmi $(docker images -q -a)
+sudo docker image prune --all --force
+
+# # ghcup の削除
+# sudo rm -rf /usr/local/.ghcup
+#
+# # Swift の削除
+# sudo rm -rf /usr/share/swift
+#
+# # Boost の削除
+# sudo rm -rf /usr/local/share/boost
+#
+# # .Net Core の削除
+# sudo rm -rf /usr/share/dotnet
+#
+# # Haskell の削除
+# sudo rm -rf /opt/ghc
+
+# Android SDK の削除
+sudo rm -rf /usr/local/share/android-sdk
+
+df -h
