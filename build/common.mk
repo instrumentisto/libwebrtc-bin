353f487d310ec16094481fa9e6ae6b0b2b0c774f
[Bot] Instrumentisto <57999751+instrumentisto-bot@users.noreply.github.com>
2025-04-18 18:45:33 +0200

    Upgrade to 135.0.7049.52 version (#150)
    - backport upstream changes up to 94096c3f
    - add `add_deps.patch`
    - add `windows_add_deps.patch` and `windows_fix_optional.patch`
    - remove `windows_fix_abseil.patch`
    - update paths in other patches
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: evdokimovs <evdokimovs@protonmail.ch>

--- build/common.mk
+++ build/common.mk
@@ -34,9 +34,10 @@ common-patch:
 	echo "apply patches ..." \
 	&& cd $(SRC_DIR) \
 	&& patch -p1 < $(PATCH_DIR)/nacl_armv6_2.patch \
-	&& patch -p2 < $(PATCH_DIR)/add_licenses.patch \
-	&& patch -p2 < $(PATCH_DIR)/4k.patch \
-	&& patch -p2 < $(PATCH_DIR)/fix_disable_proxy_trace_events.patch
+	&& patch -p1 < $(PATCH_DIR)/4k.patch \
+	&& patch -p2 < $(PATCH_DIR)/fix_disable_proxy_trace_events.patch \
+	&& patch -p1 < $(PATCH_DIR)/add_licenses.patch \
+	&& patch -p1 < $(PATCH_DIR)/add_deps.patch
 
 .PHONY: common-package
 common-package: copy
